9ef9eebeefd499200811cd5aaad35790f2265ae4
david-rhp <davidrhmeyer@gmail.com>
2019-09-05 21:15:11 +0200

    Initial commit

---
+++ src/main/java/com/youtubestatisticsaggregator/YoutubeStatisticsAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.youtubestatisticsaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeStatisticsAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeStatisticsAggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youtubestatisticsaggregator/YoutubeStatisticsAggregatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.youtubestatisticsaggregator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YoutubeStatisticsAggregatorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
